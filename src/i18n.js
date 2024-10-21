1f1897c1e899a306d788b057741e9fb1c6977539
Tiago <tiago.abreu.laibacas@gmail.com>
2024-10-21 19:55:24 +0100

    🎨 Improved i18next logic

--- src/i18n.js
+++ src/i18n.js
@@ -1,11 +1,28 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import HttpBackend from "i18next-http-backend";
-import LanguageDetector from "i18next-browser-languagedetector";
+
+const CustomLanguageDetector = {
+  type: "languageDetector",
+  async: true,
+  detect: (callback) => {
+    const cookieLanguage = document.cookie
+      .split("; ")
+      .find((row) => row.startsWith("i18next="));
+    const language = cookieLanguage
+      ? cookieLanguage.split("=")[1]
+      : navigator.language.split("-")[0];
+    callback(language);
+  },
+  init: () => {},
+  cacheUserLanguage: (language) => {
+    document.cookie = `i18next=${language}; path=/`;
+  },
+};
 
 i18n
   .use(HttpBackend)
-  .use(LanguageDetector)
+  .use(CustomLanguageDetector)
   .use(initReactI18next)
   .init({
     fallbackLng: "pt",
