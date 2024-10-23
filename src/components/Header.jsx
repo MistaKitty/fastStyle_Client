59ab75b2018dc0ff4f5b4ecccc21e43d8d14e99e
Tiago <tiago.abreu.laibacas@gmail.com>
2024-10-23 17:40:14 +0100

    🔄 Renamed HeaderMobile.jsx to MobileHeader.jsx, passed testing and is still a work in progress

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,3 @@
-// eslint-disable-next-line no-unused-vars
 import React, { useEffect, useState, useRef } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { useTranslation } from "react-i18next";
@@ -11,6 +10,8 @@ import {
   InputLabel,
   Select,
   MenuItem,
+  useMediaQuery,
+  createTheme,
   Typography,
 } from "@mui/material";
 import PersonAddIcon from "@mui/icons-material/PersonAdd";
@@ -23,8 +24,11 @@ import {
   handleClick,
   updateActiveButtonStyles,
 } from "../utils/Handlers";
+import MobileHeader from "./MobileHeader";
 
 export default function Header() {
+  const theme = createTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
   const buttonRefs = useRef([]);
   const location = useLocation();
   const [activeIndex, setActiveIndex] = useState(null);
@@ -77,102 +81,106 @@ export default function Header() {
 
   return (
     <AppBar position="fixed">
-      <Toolbar className="d-flex justify-content-between">
-        <Box>
-          <Link to="/" className="text-reset">
-            <img
-              src={favicon}
-              alt="Logo"
-              style={{ maxWidth: "50px", height: "auto" }}
-            />
-            <Typography
-              variant="h6"
-              sx={{ marginTop: "8px", fontSize: "16px" }}
-            >
-              Hair Radiant
-            </Typography>
-          </Link>
-        </Box>
-        <Box>
-          {items.map((item, index) => (
-            <Link
-              to={item.path}
-              key={index}
-              style={{ textDecoration: "none" }}
-              className="text-reset"
-            >
-              <Button
-                ref={(el) => (buttonRefs.current[index] = el)}
-                color="inherit"
-                onMouseEnter={() => handleMouseEnter(index)}
-                onMouseLeave={() => handleMouseLeave(index)}
-                onClick={() => handleClick(index)}
-                style={{
-                  backgroundColor:
-                    location.pathname === item.path || index === activeIndex
-                      ? "rgba(0, 0, 0, 0.2)"
-                      : "",
-                  transition: "background-color 0.2s",
-                }}
+      {isMobile ? (
+        <MobileHeader />
+      ) : (
+        <Toolbar className="d-flex justify-content-between">
+          <Box>
+            <Link to="/" className="text-reset">
+              <img
+                src={favicon}
+                alt="Logo"
+                style={{ maxWidth: "50px", height: "auto" }}
+              />
+              <Typography
+                variant="h6"
+                sx={{ marginTop: "8px", fontSize: "16px" }}
               >
-                {item.t}
-              </Button>
-            </Link>
-          ))}
-        </Box>
-        <Box>
-          <FormControl
-            variant="filled"
-            size="small"
-            sx={{ m: 1, minWidth: 120 }}
-          >
-            <InputLabel
-              id="LangSelectorLabel"
-              sx={{ textAlign: "center", width: "100%" }}
-            >
-              {t("botao.lingua")}
-            </InputLabel>
-            <Select
-              labelId="LangSelectorLabel"
-              id="LangSelector"
-              onChange={(event) =>
-                handleLanguageChange(event, i18n, setLanguage)
-              }
-              value={language}
-            >
-              {languages.map((lang) => (
-                <MenuItem key={lang.code} value={lang.code}>
-                  {lang.name}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-          <Box sx={{ m: 1, display: "flex", gap: 1 }}>
-            <Link to="/registar" style={{ textDecoration: "none", flex: 1 }}>
-              <LoadingButton
-                size="small"
-                loadingPosition="start"
-                loadingIndicator="botao.registando"
-                startIcon={<PersonAddIcon />}
-                variant="contained"
-              >
-                {t("botao.registar")}
-              </LoadingButton>
-            </Link>
-            <Link to="/login" style={{ textDecoration: "none", flex: 1 }}>
-              <LoadingButton
-                size="small"
-                loadingPosition="start"
-                loadingIndicator="botao.logando"
-                startIcon={<PersonAddIcon />}
-                variant="contained"
-              >
-                {t("botao.logar")}
-              </LoadingButton>
+                Hair Radiant
+              </Typography>
             </Link>
           </Box>
-        </Box>
-      </Toolbar>
+          <Box>
+            {items.map((item, index) => (
+              <Link
+                to={item.path}
+                key={index}
+                style={{ textDecoration: "none" }}
+                className="text-reset"
+              >
+                <Button
+                  ref={(el) => (buttonRefs.current[index] = el)}
+                  color="inherit"
+                  onMouseEnter={() => handleMouseEnter(index)}
+                  onMouseLeave={() => handleMouseLeave(index)}
+                  onClick={() => handleClick(index)}
+                  style={{
+                    backgroundColor:
+                      location.pathname === item.path || index === activeIndex
+                        ? "rgba(0, 0, 0, 0.2)"
+                        : "",
+                    transition: "background-color 0.2s",
+                  }}
+                >
+                  {item.t}
+                </Button>
+              </Link>
+            ))}
+          </Box>
+          <Box>
+            <FormControl
+              variant="filled"
+              size="small"
+              sx={{ m: 1, minWidth: 120 }}
+            >
+              <InputLabel
+                id="LangSelectorLabel"
+                sx={{ textAlign: "center", width: "100%" }}
+              >
+                {t("botao.lingua")}
+              </InputLabel>
+              <Select
+                labelId="LangSelectorLabel"
+                id="LangSelector"
+                onChange={(event) =>
+                  handleLanguageChange(event, i18n, setLanguage)
+                }
+                value={language}
+              >
+                {languages.map((lang) => (
+                  <MenuItem key={lang.code} value={lang.code}>
+                    {lang.name}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+            <Box sx={{ m: 1, display: "flex", gap: 1 }}>
+              <Link to="/registar" style={{ textDecoration: "none", flex: 1 }}>
+                <LoadingButton
+                  size="small"
+                  loadingPosition="start"
+                  loadingIndicator="botao.registando"
+                  startIcon={<PersonAddIcon />}
+                  variant="contained"
+                >
+                  {t("botao.registar")}
+                </LoadingButton>
+              </Link>
+              <Link to="/login" style={{ textDecoration: "none", flex: 1 }}>
+                <LoadingButton
+                  size="small"
+                  loadingPosition="start"
+                  loadingIndicator="botao.logando"
+                  startIcon={<PersonAddIcon />}
+                  variant="contained"
+                >
+                  {t("botao.logar")}
+                </LoadingButton>
+              </Link>
+            </Box>
+          </Box>
+        </Toolbar>
+      )}
     </AppBar>
   );
 }
